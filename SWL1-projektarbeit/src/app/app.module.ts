effe97842eb41f08c632dc12a3eb024e1ca53b6b
bones0 <bones0@p-guhl.ch>
2019-10-28 11:35:12 +0100

    Für Story 4 Komponente erstellt

--- SWL1-projektarbeit/src/app/app.module.ts
+++ SWL1-projektarbeit/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { ChatBarComponent } from './chat-bar/chat-bar.component';
+import { ChatHistoryComponent } from './chat-history/chat-history.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ChatBarComponent } from './chat-bar/chat-bar.component';
     HeaderComponent,
     MainComponent,
     FooterComponent,
-    ChatBarComponent
+    ChatBarComponent,
+    ChatHistoryComponent
   ],
   imports: [
     BrowserModule,
