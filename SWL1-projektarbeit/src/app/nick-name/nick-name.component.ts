82e311ad37851a5583bde61dae1aeeb2774a8e4a
bones0 <bones0@p-guhl.ch>
2019-11-05 11:01:46 +0100

    Component nick-name erstellt (leer)

---
+++ SWL1-projektarbeit/src/app/nick-name/nick-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nick-name',
+  templateUrl: './nick-name.component.html',
+  styleUrls: ['./nick-name.component.css']
+})
+export class NickNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
