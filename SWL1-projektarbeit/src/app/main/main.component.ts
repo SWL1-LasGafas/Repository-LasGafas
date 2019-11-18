8cd7b6c166acdcb03a652130a895f9f06769fd55
bones0 <bones0@p-guhl.ch>
2019-11-18 15:09:46 +0100

    code aufräumen

--- SWL1-projektarbeit/src/app/main/main.component.ts
+++ SWL1-projektarbeit/src/app/main/main.component.ts
@@ -15,12 +15,6 @@ export class MainComponent implements OnInit {
   nickName:string="";
   nickSet:boolean=false;
 
-  scrollTop() {
-    var pos=document.getElementById("historyContainer").scrollHeight;
-    console.log("Scrolling down to "+pos);
-    document.getElementById("historyContainer").scrollTop=pos;
-  }
-
   ngOnInit() {
   }
 
@@ -31,9 +25,8 @@ export class MainComponent implements OnInit {
 
   chatMsg(event: any): void {
     // console.log(<string>event.toUpperCase()); // Ausgeblendet, weil das die Konsole grausam zuballert
-    this.historyText=this.historyText+<string>event;
+    this.historyText=<string>event;
     this.messageText="";
-    this.scrollTop(); // Hier ist das Problem, dass das an der Stelle zu früh ist, aber drüben auf chat-history das Ganze nicht dem entspricht, was in der Lösung gemacht wurde. Es löst keinen change-Event aus, dort.
   }
 
 }
